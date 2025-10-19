9462a700008ccf3257ced29c4c6c171ddbc6215a
YassineElkefi <yassine.elkefi6@gmail.com>
2025-10-19 12:18:57 +0100

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Yassine EL KEFI on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
